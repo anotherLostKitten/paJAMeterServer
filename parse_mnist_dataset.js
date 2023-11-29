410a177033fc0c6088a0d48eca47a739c5673d6c
anotherLostKitten <eggismagicchickensquared@gmail.com>
2023-11-28 21:24:18 -0500

    integrate data loading with jamscript script

--- parse_mnist_dataset.js
+++ parse_mnist_dataset.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-function parseMNISTDataset(setSize, imagesPath, labelsPath, outputPath) {
+function parseMNISTDataset(setSize, imagesPath, labelsPath, outputPath = "") {
     const dataFileBuffer = fs.readFileSync(imagesPath);
     const labelFileBuffer = fs.readFileSync(labelsPath);
     const pixelValues = [];
@@ -14,15 +14,27 @@ function parseMNISTDataset(setSize, imagesPath, labelsPath, outputPath) {
             }
         }
 
-        const imageData = {};
-        imageData[labelFileBuffer[image + 8]] = pixels; // Assuming labelFileBuffer contains label values directly
+        const label = labelFileBuffer[image + 8]
+
+        // const binary_classifiers = [];
+        // for (var i = 0; i < 10; i++)
+        //     binary_classifiers.push(label === i ? 1 : 0);
+
+        const imageData = [label, pixels];
 
         pixelValues.push(imageData);
     }
-
-    fs.writeFileSync(outputPath, JSON.stringify(pixelValues));
+    //console.log(pixelValues);
+    if (outputPath)
+        fs.writeFileSync(outputPath, JSON.stringify(pixelValues));
+    else
+        return pixelValues;
 }
 
+// The parsed data looks like:
+//  [ [0, 0, 1, ..., 0], -- 10 entries, binary classifiers
+//    [0, 0, 0, 0, 0, 0, 0, 0, ... ] -- image data
+//  ]
 
 const trainingSetSize = 60000; // Number of examples in the training set
 const testSetSize = 10000; // Number of examples in the test set
@@ -31,12 +43,24 @@ const trainingLabelsPath = __dirname + '/training_sets/train-labels.idx1-ubyte';
 const testImagesPath = __dirname + '/training_sets/t10k-images.idx3-ubyte';
 const testLabelsPath = __dirname + '/training_sets/t10k-labels.idx1-ubyte';
 
-// Generate parsed JSON for training set
-parseMNISTDataset(trainingSetSize, trainingImagesPath, trainingLabelsPath, __dirname + '/training_sets/parsed_mnist_training_set.json');
 
-// Generate parsed JSON for test set
-parseMNISTDataset(testSetSize, testImagesPath, testLabelsPath, __dirname + '/training_sets/parsed_mnist_test_set.json');
+function writeDataToFile() {
+    // Generate parsed JSON for training set
+    parseMNISTDataset(trainingSetSize, trainingImagesPath, trainingLabelsPath, __dirname + '/training_sets/parsed_mnist_training_set.json');
 
-// The parsed data looks like:
-//  '5': [
-//   0, 0, 0, 0, 0, 0, 0, 0, ...
+    // Generate parsed JSON for test set
+    parseMNISTDataset(testSetSize, testImagesPath, testLabelsPath, __dirname + '/training_sets/parsed_mnist_test_set.json');
+}
+
+function loadTrainingData() {
+    return  parseMNISTDataset(trainingSetSize, trainingImagesPath, trainingLabelsPath);
+}
+function loadTestingData() {
+    return parseMNISTDataset(testSetSize, testImagesPath, testLabelsPath);
+}
+
+
+module.exports = {
+    loadTrainingData: loadTrainingData,
+    loadTestingData: loadTestingData,
+};
